ebd8faabdaaa7a773a48f3c9435fa96ccb9e86b0
totomanov <totomanov@protonmail.com>
2023-11-06 17:52:59 +0200

    feat: ChainlinkAdapter

---
+++ src/adapters/ImmutableUniswapV3Adapter.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.21;
+
+import {FeedRegistryInterface} from "@chainlink/interfaces/FeedRegistryInterface.sol";
+
+contract ImmutableUniswapV3Adapter {}
