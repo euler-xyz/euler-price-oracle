a8631f51aa93a6740513692ed0ce54ddd2797efe
totomanov <totomanov@protonmail.com>
2023-11-17 20:05:27 +0300

    feat: ConstantRateOracle

---
+++ src/simple/ConstantRateOracle.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.21;
+
+contract ConstantRateOracle {
+    uint256 public constant PRECISION_DECIMALS = 27;
+    uint256 public immutable maxStaleness;
+
+    mapping(address base => mapping(address quote => uint256 rate)) public configs;
+
+    error AlreadyConfigured(address base, address quote);
+    error ArityMismatch(uint256 arityA, uint256 arityB, uint256 arityC);
+    error NotConfigured(address base, address quote);
+
+    constructor(address[] memory bases, address[] memory quotes, uint256[] memory rates) {
+        if (bases.length != quotes.length || quotes.length != rates.length) {
+            revert ArityMismatch(bases.length, quotes.length, rates.length);
+        }
+
+        uint256 length = bases.length;
+        for (uint256 i = 0; i < length;) {
+            _initConfig(bases[i], quotes[i], rates[i]);
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    function getQuote(uint256 inAmount, address base, address quote) external view returns (uint256) {
+        uint256 rate = _getOrRevertConfig(base, quote);
+        uint256 price = inAmount * rate / 10 ** PRECISION_DECIMALS;
+        return price;
+    }
+
+    function getQuotes(uint256 inAmount, address base, address quote) external view returns (uint256, uint256) {
+        uint256 rate = _getOrRevertConfig(base, quote);
+        uint256 price = inAmount * rate / 10 ** PRECISION_DECIMALS;
+        return (price, price);
+    }
+
+    function _initConfig(address base, address quote, uint256 rate) internal {
+        if (configs[base][quote] != 0) revert AlreadyConfigured(base, quote);
+        configs[base][quote] = rate;
+    }
+
+    function _getOrRevertConfig(address base, address quote) internal view returns (uint256) {
+        uint256 rate = configs[base][quote];
+        if (rate == 0) revert NotConfigured(base, quote);
+        return rate;
+    }
+}
