ed7a86bcd57ea283d4d5f6ac6d25111b9bae5f13
totomanov <totomanov@protonmail.com>
2023-11-19 16:30:27 +0300

    feat: refactor aggregators

---
+++ src/aggregators/Aggregator.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.22;
+
+import {IOracle} from "src/interfaces/IOracle.sol";
+import {FixedSizeAddressArray} from "src/lib/FixedSizeAddressArray.sol";
+
+abstract contract Aggregator is FixedSizeAddressArray {
+    uint256 public immutable quorum;
+
+    error QuorumNotReached(uint256 count, uint256 quorum);
+    error QuorumTooLarge(uint256 quorum, uint256 maxQuorum);
+    error QuorumZero();
+
+    constructor(address[] memory _oracles, uint256 _quorum) FixedSizeAddressArray(_oracles) {
+        if (_quorum == 0) revert QuorumZero();
+        if (_quorum > cardinality) revert QuorumTooLarge(_quorum, cardinality);
+
+        quorum = _quorum;
+    }
+
+    function getQuote(uint256 inAmount, address base, address quote) external view returns (uint256) {
+        uint256[] memory answers = new uint256[](cardinality);
+        uint256 numAnswers;
+
+        for (uint256 i = 0; i < cardinality;) {
+            IOracle oracle = IOracle(_get(i));
+            (bool success, uint256 answer) = _tryOracle(oracle, inAmount, base, quote);
+
+            unchecked {
+                if (success) {
+                    answers[numAnswers] = answer;
+                    numAnswers++;
+                }
+                ++i;
+            }
+        }
+
+        if (numAnswers < quorum) revert QuorumNotReached(numAnswers, quorum);
+
+        assembly {
+            // update the length of answer
+            // this is safe because new length <= initial length
+            mstore(answers, numAnswers)
+        }
+
+        // aggregation logic - sort answers and return the median
+        return _aggregateQuotes(answers);
+    }
+
+    function _aggregateQuotes(uint256[] memory quotes) internal view virtual returns (uint256);
+
+    function _tryOracle(IOracle oracle, uint256 inAmount, address base, address quote)
+        private
+        view
+        returns (bool, /* success */ uint256 /* outAmount */ )
+    {
+        try oracle.getQuote(inAmount, base, quote) returns (uint256 outAmount) {
+            return (true, outAmount);
+        } catch {
+            return (false, 0);
+        }
+    }
+}
--- src/aggregators/MinAggregator.sol
+++ src/aggregators/MinAggregator.sol
@@ -4,18 +4,12 @@ pragma solidity 0.8.22;
 import {LibSort} from "@solady/utils/LibSort.sol";
 import {Aggregator} from "src/aggregators/Aggregator.sol";
 
-contract MedianAggregator is Aggregator {
+contract MinAggregator is Aggregator {
     constructor(address[] memory _oracles, uint256 _quorum) Aggregator(_oracles, _quorum) {}
 
     function _aggregateQuotes(uint256[] memory quotes) internal pure override returns (uint256) {
-        // sort and return the median
+        // return and return the lowest quote
         LibSort.insertionSort(quotes);
-        uint256 size = quotes.length;
-        uint256 midpoint = size / 2;
-        if (size % 2 == 0) {
-            return quotes[midpoint];
-        } else {
-            return (quotes[midpoint] + quotes[midpoint - 1]) / 2;
-        }
+        return quotes[0];
     }
 }
