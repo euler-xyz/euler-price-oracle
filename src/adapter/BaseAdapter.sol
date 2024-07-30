1558b2f5c2b874932c9ccdbf77592e50b1b52f74
totomanov <totomanov@protonmail.com>
2024-07-30 17:40:41 +0300

    fix: cantina-118

--- src/adapter/BaseAdapter.sol
+++ src/adapter/BaseAdapter.sol
@@ -10,6 +10,10 @@ import {Errors} from "src/lib/Errors.sol";
 /// @author Euler Labs (https://www.eulerlabs.com/)
 /// @notice Abstract adapter with virtual bid/ask pricing.
 abstract contract BaseAdapter is IPriceOracle {
+    // @dev Addresses <= 0x00..00ffffffff are considered to have 18 decimals without dispatching a call.
+    // This avoids collisions between ISO 4217 representations and (future) precompiles.
+    uint256 internal constant ADDRESS_RESERVED_RANGE = 0xffffffff;
+
     /// @inheritdoc IPriceOracle
     function getQuote(uint256 inAmount, address base, address quote) external view returns (uint256) {
         return _getQuote(inAmount, base, quote);
@@ -22,14 +26,16 @@ abstract contract BaseAdapter is IPriceOracle {
         return (outAmount, outAmount);
     }
 
-    /// @notice Call `decimals()`, falling back to 18 decimals.
+    /// @notice Determine the decimals of an asset.
     /// @param asset ERC20 token address or other asset.
     /// @dev Oracles can use ERC-7535, ISO 4217 or other conventions to represent non-ERC20 assets as addresses.
     /// Integrator Note: `_getDecimals` will return 18 if `asset` is:
+    /// - any address <= 0x00000000000000000000000000000000ffffffff (4294967295)
     /// - an EOA or a to-be-deployed contract (which may implement `decimals()` after deployment).
     /// - a contract that does not implement `decimals()`.
     /// @return The decimals of the asset.
     function _getDecimals(address asset) internal view returns (uint8) {
+        if (uint160(asset) <= ADDRESS_RESERVED_RANGE) return 18;
         (bool success, bytes memory data) = asset.staticcall(abi.encodeCall(IERC20.decimals, ()));
         return success && data.length == 32 ? abi.decode(data, (uint8)) : 18;
     }
--- test/adapter/BaseAdapter.t.sol
+++ test/adapter/BaseAdapter.t.sol
@@ -7,14 +7,21 @@ import {BaseAdapterHarness} from "test/adapter/BaseAdapterHarness.sol";
 import {boundAddr} from "test/utils/TestUtils.sol";
 
 contract BaseAdapterTest is Test {
+    uint160 internal constant ADDRESS_RESERVED_RANGE = 0xffffffff;
     BaseAdapterHarness oracle;
 
     function setUp() public {
         oracle = new BaseAdapterHarness();
     }
 
+    function test_GetDecimals_Integrity_ReservedRange(address x) public view {
+        x = address(uint160(x) % ADDRESS_RESERVED_RANGE);
+        assertEq(oracle.getDecimals(x), 18);
+    }
+
     function test_GetDecimals_Integrity_ERC20(address x, uint8 decimals) public {
         x = boundAddr(x);
+        vm.assume(uint160(x) > ADDRESS_RESERVED_RANGE);
         vm.mockCall(x, abi.encodeWithSelector(IERC20.decimals.selector), abi.encode(decimals));
 
         uint8 _decimals = oracle.getDecimals(x);
@@ -23,6 +30,7 @@ contract BaseAdapterTest is Test {
 
     function test_GetDecimals_Integrity_nonERC20(address x) public view {
         x = boundAddr(x);
+        vm.assume(uint160(x) > ADDRESS_RESERVED_RANGE);
 
         uint8 decimals = oracle.getDecimals(x);
         assertEq(decimals, 18);
