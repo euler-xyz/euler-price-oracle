4f8183c2604932961595db808eae5f1609f9ec9c
totomanov <totomanov@protonmail.com>
2024-09-04 21:00:50 +0300

    chore: unused file

--- test/utils/ForkTest.sol
+++ test/utils/ForkTest.sol
@@ -6,7 +6,6 @@ import {Test} from "forge-std/Test.sol";
 contract ForkTest is Test {
     uint256 constant ETHEREUM_FORK_BLOCK = 18888888;
     uint256 ethereumFork;
-    uint256 arbitrumFork;
 
     function _setUpFork() public {
         _setUpFork(ETHEREUM_FORK_BLOCK);
@@ -21,16 +20,5 @@ contract ForkTest is Test {
         string memory ETHEREUM_RPC_URL = vm.envString("ETHEREUM_RPC_URL");
         ethereumFork = vm.createFork(ETHEREUM_RPC_URL);
         vm.selectFork(ethereumFork);
-    }
-
-    function _setUpArbitrumFork(uint256 blockNumber) public {
-        _setUpArbitrumForkLatest();
-        vm.rollFork(blockNumber);
-    }
-
-    function _setUpArbitrumForkLatest() public {
-        string memory ARBITRUM_RPC_URL = vm.envString("ARBITRUM_RPC_URL");
-        arbitrumFork = vm.createFork(ARBITRUM_RPC_URL);
-        vm.selectFork(arbitrumFork);
     }
 }
