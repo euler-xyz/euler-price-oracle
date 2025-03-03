fc8e0c37e137038925f539e329744b3bf250d9a7
totomanov <totomanov@protonmail.com>
2025-03-03 17:44:15 +0200

    feat: ERC4626 and Sentinel

--- test/StubERC4626.sol
+++ test/StubERC4626.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.0;
 
+import {console2} from "forge-std/console2.sol";
+
 contract StubERC4626 {
-    address public asset;
+    address public immutable asset;
     uint256 private rate;
     string revertMsg = "oops";
     bool doRevert;
@@ -22,11 +24,13 @@ contract StubERC4626 {
 
     function convertToAssets(uint256 shares) external view returns (uint256) {
         if (doRevert) revert(revertMsg);
+        console2.log("convertToAssets", shares, rate);
         return shares * rate / 1e18;
     }
 
     function convertToShares(uint256 assets) external view returns (uint256) {
         if (doRevert) revert(revertMsg);
+        console2.log("convertToShares", assets, rate);
         return assets * 1e18 / rate;
     }
 }
