43e484a4ce0511b4377a3f87a0e0512502407437
totomanov <totomanov@protonmail.com>
2025-03-03 17:57:21 +0200

    rm: debug prints

--- test/StubERC4626.sol
+++ test/StubERC4626.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.0;
 
-import {console2} from "forge-std/console2.sol";
-
 contract StubERC4626 {
     address public immutable asset;
     uint256 private rate;
@@ -24,13 +22,11 @@ contract StubERC4626 {
 
     function convertToAssets(uint256 shares) external view returns (uint256) {
         if (doRevert) revert(revertMsg);
-        console2.log("convertToAssets", shares, rate);
         return shares * rate / 1e18;
     }
 
     function convertToShares(uint256 assets) external view returns (uint256) {
         if (doRevert) revert(revertMsg);
-        console2.log("convertToShares", assets, rate);
         return assets * 1e18 / rate;
     }
 }
