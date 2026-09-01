dba0a90aa2de064260e1150f5e5395b213cfb5a5
kasperpawlowski <kasperpawlowski@gmail.com>
2026-09-01 12:12:39 +0200

    test: adapt redstone FFI script to evm-connector 0.9.0 API
    DataServiceWrapper in 0.9.0 renamed dataFeeds to dataPackagesIds and
    made authorizedSigners required; supply them via
    getSignersForDataServiceId from @redstone-finance/sdk, added as a
    direct dependency since the script now requires it.

--- test/adapter/redstone/get_redstone_payload.js
+++ test/adapter/redstone/get_redstone_payload.js
@@ -1,11 +1,13 @@
 const {DataServiceWrapper} = require("@redstone-finance/evm-connector");
+const {getSignersForDataServiceId} = require("@redstone-finance/sdk");
 
 async function getPayload() {
   const feed = process.argv[2];
   const wrapper = new DataServiceWrapper({
     dataServiceId: "redstone-primary-prod",
-    dataFeeds: [feed],
-    uniqueSignersCount: 3
+    dataPackagesIds: [feed],
+    uniqueSignersCount: 3,
+    authorizedSigners: getSignersForDataServiceId("redstone-primary-prod")
   });
   const redstonePayload = await wrapper.getBytesDataForAppending();
   console.log(redstonePayload);
